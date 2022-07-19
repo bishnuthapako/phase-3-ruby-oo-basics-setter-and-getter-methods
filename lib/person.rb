32fe2d42c0171e8c3ae72b1a448a2eb81ba229ec
Bishnu <bishnuthapako@gmail.com>
2022-07-19 14:38:13 -0400

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,36 @@
+require "pry"
+
+
+# class Person
+#     attr_reader :name
+#     attr_accessor :job
+
+#     def initialize(name, job)
+#         @name=name
+#         @job=job
+#     end
+# end
+# r1 = Person.new("Bishnu", "IT")
+
+
 class Person
 
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+    def job
+        @job
+    end
+
 end
+r1= Person.new
+r1.name = "bishnu"
+r1.job = 'Developer'
+binding.pry
